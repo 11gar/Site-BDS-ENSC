c574d628a3b78df2015026af89a5ec10f9c16d57
11gar <wetzel.ansgar@gmail.com>
2023-07-08 03:31:40 +0200

    Joli classement

--- client/src/Pages/prewei-page/prewei-page.tsx
+++ client/src/Pages/prewei-page/prewei-page.tsx
@@ -33,13 +33,13 @@ export default class PreweiPage extends React.Component {
     await this.getTeamsFromService();
     const lengths: number[] = [];
     this.state.teams.forEach((team) => {
-      let length = 0;
-      if (team.points < 0) {
-        length = 0;
+      let length = "";
+      if ((team.points * 100) / this.state.teams[0].points < 3) {
+        length = "25px";
       } else {
-        length = (team.points * 100) / this.state.teams[0].points;
+        length = (team.points * 100) / this.state.teams[0].points + "%";
       }
-      document.getElementById(team.id.toString())!.style.width = length + "%";
+      document.getElementById(team.id.toString())!.style.width = length;
     });
   }
 
@@ -64,22 +64,79 @@ export default class PreweiPage extends React.Component {
         }
       );
     });
+    this.state.teams.forEach((team, index) => {
+      console.log("coucou");
+      const elem = document.getElementById("name" + team.id.toString());
+      elem!.animate(
+        [
+          // étapes/keyframes
+
+          { opacity: "0" },
+          { opacity: "1" },
+        ],
+        {
+          // temporisation
+          duration: 1000,
+          easing: "ease-in-out",
+          delay: nbTeams - index * 100 + 500,
+          fill: "both",
+        }
+      );
+    });
+    this.state.teams.forEach((team, index) => {
+      console.log("coucou");
+      const elem = document.getElementById("cover" + team.id.toString());
+      elem!.animate(
+        [
+          // étapes/keyframes
+          { transform: "translateX(50%)" },
+          { transform: "translateX(0%)" },
+        ],
+        {
+          // temporisation
+          duration: 1000,
+          easing: "ease-in-out",
+          delay: nbTeams - index * 100 + 500,
+          fill: "both",
+        }
+      );
+    });
   }
 
   render() {
     return (
       <div className="prewei page">
-        <div id="caca" className="teams">
-          {this.state.teams.map((team, index) => (
-            <div className="team">
-              <div className="points-container">
-                <div id={team.id.toString()} className={"points p" + index}>
-                  <div className="points-text">{team.points}</div>
+        <div className="title">
+          <h1>Classement Actuel</h1>
+        </div>
+        <div className="classement">
+          <div className="teams">
+            {this.state.teams.map((team, index) => (
+              <div className="team">
+                <div className="points-container">
+                  <div id={team.id.toString()} className={"points p" + index}>
+                    <div className="points-text">{team.points}</div>
+                  </div>
+                </div>
+                <div id={"name" + team.id.toString()} className="team-nom">
+                  <img
+                    src={require("../../Assets/Images/equipes/" +
+                      team.nom +
+                      ".jpg")}
+                  />
+                  <div
+                    id={"cover" + team.id.toString()}
+                    className="cover"
+                  ></div>
+                  <div className="txt">{team.nom}</div>
                 </div>
               </div>
-              <div className="team-nom">{team.nom}</div>
-            </div>
-          ))}
+            ))}
+          </div>
+        </div>
+        <div className="boutons">
+          <div className="bouton">+ Remplir un défi</div>
+          <div className="bouton light">Liste des défis</div>
         </div>
       </div>
     );
